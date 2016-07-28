52ffaad0580e3d679cb47d19e93133f3f065eef1
nileshgokhalepune <nileshgokhale45@gmail.com>
2016-07-29 02:02:21 +0530

    Added access_token logic to get the twitter token and user details

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var qs = require('querystring');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Social Network' });
@@ -11,22 +12,25 @@ router.get('/', function (req, res, next) {
 router.get('/twitter/callback', function (req, res, next) {
   console.log(req.query.oauth_verifier);
   var data = qs.parse(req.query);
-  
+
   request.post({
     url: 'https://api.twitter.com/oauth/access_token',
-    method:'POST',
-    headers:{
+    method: 'POST',
+    form: {
       'oauth_token': req.query.oauth_token,
       'oauth_verifier': req.query.oauth_verifier
-    } 
-  }, function(e, resp, body){
-    res.render('auth', { authToken: "Test" +  body });
+    }
+  }, function (e, resp, body) {
+    var parms = qs.parse(body);
+    console.log(parms);
+    res.render('auth', { authToken: body });
+
   });
-  
+
 });
 
 router.get('/social/:t/:v', function (req, res, next) {
-  res.render('index', {});
+  res.render('index', { title: res });
 });
 
 module.exports = router;
